c1a6391768ed30bb02339934e59171d8eb49bcc9
mhashir1010 <m.hashir10102@gmail.com>
2021-07-15 21:55:54 +0500

    Auth Component

--- client/src/app/auth/auth.component.ts
+++ client/src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-
-  constructor() { }
-
+  isLogin:boolean=true;
+  isSubmitting = false;
+  authForm: FormGroup;
+  constructor(private router: Router,private fb: FormBuilder)
+  {
+    this.authForm = this.fb.group({'email': ['', Validators.required],'password': ['', Validators.required]});
+  }
   ngOnInit(): void {
   }
-
+  submitForm()
+  {
+    console.log(this.authForm.value);
+    
+  }
+  addFormControl()
+  {
+    console.log(this.isLogin)
+    this.isLogin=!this.isLogin;
+    if (!this.isLogin) 
+    {
+      this.authForm.addControl('fname', new FormControl());
+      this.authForm.addControl('lname', new FormControl());
+      this.authForm.addControl('degree', new FormControl());
+      this.authForm.addControl('campus', new FormControl());
+    }
+  }
 }
