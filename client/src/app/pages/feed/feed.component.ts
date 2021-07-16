a43b75d269087e7d4a491b3461adccc6ec7ca27d
Husnain <husnain.butt300@gmail.com>
2021-07-16 15:21:02 +0500

    modal, ad post

--- client/src/app/pages/feed/feed.component.ts
+++ client/src/app/pages/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-feed',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
   active = 1;
-  constructor() {}
+  closeResult = '';
 
-  ngOnInit(): void {}
+  constructor(private modalService: NgbModal) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  open(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
