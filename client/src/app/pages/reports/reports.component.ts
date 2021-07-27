74d94f0c7945a1ccc5eb981fe9e6abb579310b7c
mhashir1010 <m.hashir10102@gmail.com>
2021-07-27 22:54:24 +0500

    report

--- client/src/app/pages/reports/reports.component.ts
+++ client/src/app/pages/reports/reports.component.ts
@@ -9,16 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
   reports!:Report[];
-  reportType!:number;
-  constructor(private reportSerice:ReportService) { }
+  reportType='Users';
+  report=['Users','Posts','Community']
+  constructor(private reportService:ReportService) { }
 
   ngOnInit(): void {
-    this.reportSerice.getAllReports().subscribe
+    this.reportService.getAllReports(0).subscribe
     (
       res=>{
         this.reports=res.data.reports.docs;
       }
     )
   }
-
+  onChange(type:number)
+  {
+    console.log(this.reportType)
+    if(type>0){
+       this.reportService.getAllReports(type).subscribe(res=>{this.reports=res.data.reports.docs});
+       this.reportType=this.report[type];
+       return this.reports
+    }
+    else
+    {
+      this.reportService.getAllReports(0).subscribe(res=>{this.reports=res.data.reports.docs});
+      return this.reports
+    }
+  }
 }
