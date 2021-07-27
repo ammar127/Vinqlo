2f620216c2608f37631a2cd672684da96591ef07
Ammar Iqbal <ammariqbal127@gmail.com>
2021-07-27 19:40:05 +0500

    adding auth

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
@@ -10,6 +11,7 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [AuthGuard],
     children: [
 
       {
