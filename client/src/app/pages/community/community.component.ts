fdd7e6f91837d76e9ae78b00ab4987cd302c779e
Husnain <husnain.butt300@gmail.com>
2021-07-30 17:08:46 +0500

    add community

--- client/src/app/pages/community/community.component.ts
+++ client/src/app/pages/community/community.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from './../../core/services/user.service';
 @Component({
   selector: 'app-community',
   templateUrl: './community.component.html',
-  styleUrls: ['./community.component.css']
+  styleUrls: ['./community.component.css'],
 })
 export class CommunityComponent implements OnInit {
+  active = 1;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  feedpath = '/posts/get/feed';
+  academicesPath = '/communities/get/academics';
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {}
+  get user() {
+    return this.userService.getCurrentUser();
   }
-
 }
