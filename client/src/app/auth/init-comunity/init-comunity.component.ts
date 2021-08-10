c29d48ae4e69b1ade29d3cd36ea2d7811e697929
Husnain <husnain.butt300@gmail.com>
2021-08-10 18:38:10 +0500

    add design changes in admin panel

--- client/src/app/auth/init-comunity/init-comunity.component.ts
+++ client/src/app/auth/init-comunity/init-comunity.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class InitComunityComponent implements OnInit {
 
   constructor(private router:Router) { }
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   ngOnInit(): void {
   }
   checkClick(){
