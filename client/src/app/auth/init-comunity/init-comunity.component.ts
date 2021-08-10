7ad279151702c8afe898f977107df4ea1eebc43f
Husnain <husnain.butt300@gmail.com>
2021-08-11 00:54:10 +0500

    init comunity added

--- client/src/app/auth/init-comunity/init-comunity.component.ts
+++ client/src/app/auth/init-comunity/init-comunity.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./init-comunity.component.css']
 })
 export class InitComunityComponent implements OnInit {
-
+  active = 'top';
   constructor(private router:Router) { }
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   ngOnInit(): void {
