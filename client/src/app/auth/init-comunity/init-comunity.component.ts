a376d8d2bdcfe95e7cd8b7e25a1d7c7522f5230d
mhashir1010 <m.hashir10102@gmail.com>
2021-08-11 21:09:41 +0500

    Initial Communities

--- client/src/app/auth/init-comunity/init-comunity.component.ts
+++ client/src/app/auth/init-comunity/init-comunity.component.ts
@@ -1,5 +1,8 @@
+import { Toast } from './../../core/constants/Toast';
+import { CommonService } from './../../core/services/common.service';
+import { Community, CommunityService } from 'src/app/core';
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-init-comunity',
@@ -8,11 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitComunityComponent implements OnInit {
   active = 'top';
-  constructor(private router:Router) { }
+  step=1;
+  slug:string='';
+  joinSlug='';
+  communities:Community[]=[];
+  get categories()  {return this.commonService.categories(); }
+  constructor(private router:Router,private communityService: CommunityService,private commonService:CommonService) { }
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   ngOnInit(): void {
   }
+  getComunities(catSlug:string)
+  {
+    this.slug=this.categories[0].slug;
+    this.communityService.getCommunitiesByCategory('/communities/get/academics',this.slug).subscribe(res=>
+      {
+        if(res.status==200){
+          this.communities=res.data.docs;
+        }
+      })
+  }
   checkClick(){
     this.router.navigate(['/feed'])
+  }
+  onJoinClick(slug: string,isJoined:boolean) {
+    this.joinSlug = slug;
+    this.communityService.join(slug,isJoined).subscribe(res => {
+      if(res.status === 200 && !isJoined) {
+        Toast.fire({icon:'success', title: 'you joined a Community '});
+        this.communities[this.communities.findIndex(c => c.slug == slug)].isJoined=!this.communities[this.communities.findIndex(c => c.slug == slug)].isJoined;
+        this.joinSlug = '';
+      }
+      else if(res.status === 200 && isJoined){
+        Toast.fire({icon:'success', title: 'you un-joined a Community '});
+        this.communities[this.communities.findIndex(c => c.slug == slug)].isJoined=!this.communities[this.communities.findIndex(c => c.slug == slug)].isJoined;
+        this.joinSlug = '';
+      }
+   } )
   }
 }
