0aa1bd4a172b4cea8c8f62258aac6d1b24f0fdc6
mhashir1010 <m.hashir10102@gmail.com>
2021-08-11 21:11:00 +0500

    THIS NEED TO BE FIXED

--- client/src/app/auth/auth.component.ts
+++ client/src/app/auth/auth.component.ts
@@ -77,8 +77,10 @@ export class AuthComponent implements OnInit {
         if(res.status === 200) {
           if(res.data.user && !res.data.user.verified ) {
             route = '/auth/otp/'+res.data.user.email+'/1';
-          } else if(res.data.user && res.data.user.role === UserType.user){
+          } else if(res.data.user && res.data.user.verified ){
             route = '/initial-community';
+          } else if(res.data.user && res.data.user.role === UserType.user){
+            route = '/feed';
           } else if(res.data.user && res.data.user.role === UserType.admin || res.data.user.role === UserType.superAdmin  ){
             route = '/users';
           }
